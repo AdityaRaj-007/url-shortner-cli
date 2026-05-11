03b308063bab0916e8c05369398bd1ec9159d5e3
AdityaRaj-007 <iamadityaraj.2001@gmail.com>
2026-05-11 22:53:34 +0530

    implmented UrlData struct and improved the resolve command performance.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::{collections::HashMap, fs::{self, File}, io::Write, path::Path};
 use clap::{Parser, Subcommand};
+use serde::{Serialize, Deserialize};
 
 #[derive(Parser)]
 struct UrlShortner {
     #[command(subcommand)]
     command: Commands
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct UrlData {
+    original_url:String,
+    visits: u64,
 }
 
 #[derive(Subcommand)]
@@ -19,7 +26,7 @@ enum Commands {
     }
 }
 
-fn save_url_data(data: HashMap<String, String>) {
+fn save_url_data(data: HashMap<String, UrlData>) {
     let path = Path::new("urls.json");
     let display = path.display();
 
@@ -36,7 +43,7 @@ fn save_url_data(data: HashMap<String, String>) {
     }
 }
 
-fn get_all_urls() -> HashMap<String,String> {
+fn get_all_short_to_long_urls() -> HashMap<String,UrlData> {
     let path = Path::new("urls.json");
     if !path.exists() {
         return HashMap::new();
@@ -48,6 +55,29 @@ fn get_all_urls() -> HashMap<String,String> {
     };
     
     return serde_json::from_str(&data).unwrap_or_else(|_| HashMap::new())
+}
+
+fn get_all_long_to_short_urls() -> HashMap<String, String> {
+    let path = Path::new("urls.json");
+
+    if !path.exists() {
+        return HashMap::new();
+    }
+
+    let data = match fs::read_to_string(&path) {
+        Ok(content) => content,
+        Err(why) => panic!("Unable to read data from {} : {}", path.display(), why)
+    };
+
+    let urls_data: HashMap<String, UrlData> = serde_json::from_str(&data).unwrap();
+
+    let mut long_to_short = HashMap::new();
+
+    for (short, url_data) in &urls_data {
+        long_to_short.insert(url_data.original_url.clone(),short.clone());
+    }
+
+    return long_to_short
 }
 
 fn generate_random_id() -> String {
@@ -66,47 +96,44 @@ fn generate_random_id() -> String {
 fn main() {
     let cli = UrlShortner::parse();
 
-    let mut urls:HashMap<String, String> = get_all_urls();
+    let mut short_to_long:HashMap<String, UrlData> = get_all_short_to_long_urls();
+    let mut long_to_short:HashMap<String, String> = get_all_long_to_short_urls();
 
     match &cli.command {
         Commands::Shorten { url } => {
             println!("Command is to shorten the URL...");
-            let contains_key = urls.contains_key(url);
+            let contains_key = long_to_short.contains_key(url);
             let mut new_url = String::from("https://urlshortnercli/");
 
             if !contains_key {
                 let short_id = generate_random_id();
-                urls.insert(url.to_string(), short_id);
+                let url_data = UrlData {
+                    original_url: url.to_string(),
+                    visits: 0,
+                };
+                short_to_long.insert(short_id.clone(), url_data);
+                long_to_short.insert(url.to_string(),short_id);
+                save_url_data(short_to_long);
             } 
-            let key = urls.get(url);
+            let key = long_to_short.get(url);
             match key {
                 Some(short_id) => new_url.push_str(short_id),
                 None => println!("key doesn't exists")
             }          
 
-            println!("New url for {}: {}", url, new_url);
-
-            save_url_data(urls);
+            println!("New url for {}: {}", url, new_url);            
         },
         Commands::Resolve { url } => {
             println!("Command is used to get the original URL...");
             
-            let mut short_id = String::from("");
-
-            for char in url.chars().rev() {
-                if char != '/' {
-                    short_id.push(char);
-                } else {
-                    break;
-                }
-            }
+            let short_id = url.split("/").last().unwrap_or("");
 
             println!("{}", short_id);
 
-            let original_url = urls.iter().find_map(|(key, val)| if *val == short_id.chars().rev().collect::<String>() {Some(key)} else {None});
+            let url_data = short_to_long.get(short_id);
 
-            match original_url {
-                Some(url) => println!("Original url {}",url),
+            match url_data {
+                Some(data) => println!("Original url {}",data.original_url),
                 None => println!("Not a valid url")
             }
         }
